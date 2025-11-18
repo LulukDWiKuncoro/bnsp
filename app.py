745f41aac004756f72bfb9df432d9e86168f7b4c
LulukDWiKuncoro <lulu23203ti@student.nurulfikri.ac.id>
2025-11-18 08:37:56 +0000

    deploy

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return """
+    <html>
+      <head><title>BNSP</title></head>
+      <body>
+        <h1>Ujian BNSP</h1>
+        <p>Nama: Luluk Dwi Kuncoro</p>
+        <p>Layanan ini dideploy di VPS dengan Docker + CI/CD + Monitoring (Prometheus & Grafana).</p>
+      </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
